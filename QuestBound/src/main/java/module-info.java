207ded1a5d865e928d27eaea99765770c74620e3
Swirl86 <swesuzzi@gmail.com>
2026-01-20 12:42:06 +0100

    add JavaFX dependencies and plugin

--- QuestBound/src/main/java/module-info.java
+++ QuestBound/src/main/java/module-info.java
@@ -1,8 +1,14 @@
 module com.sus.questbound {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.core;
 
     opens com.sus.questbound to javafx.fxml;
+
+    opens com.sus.questbound.view to javafx.fxml;
+    opens com.sus.questbound.game.library to com.fasterxml.jackson.databind;
+    opens com.sus.questbound.model to com.fasterxml.jackson.databind;
+
     exports com.sus.questbound;
 }
